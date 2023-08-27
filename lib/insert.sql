c23f49c394d3b85e494bc35429ee558e312798d3
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-27 14:54:40 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+bears(name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 2, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 3, 'M', 'Brown', 'Rowdy', 1),
+('Tabitha', 2, 'F', 'Brown', 'Calm', 1),
+('Sergeant Brown', 2, 'M', 'Brown', 'Calm', 1),
+('Melissa', 2, 'F', 'Black', 'Calm', 1),
+('Grinch', 2, 'M', 'Brown', 'Calm', 1),
+('Wendy', 2, 'F', 'Brown', 'Calm', 1),
+(NULL, 2, 'M', 'Brown', 'Calm', 1)
